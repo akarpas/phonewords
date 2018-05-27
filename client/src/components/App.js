44741542bd2f686e65c561ce45b1c017518b288b
Andreas Karpasitis <karpasitis@outlook.com>
2018-05-27 20:32:19 +0200

    connect tNine action to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
+import * as actionsT9 from '../actions/tNine'
 import style from './App.scss'
 class App extends Component {
+
+  componentWillReceiveProps = (nextProps) => {  }
+
+  click = (e) => {
+    e.preventDefault
+    actionsT9.getWords(this.props.dispatch, 9)
+  }
+
   render() {
     return (
       <div>
-        <h2 className={style.title}> Phone Words </h2>
+        <h2 className={style.title} onClick={(e) => this.click(e)}> Phone Words </h2>
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    results: state.tNine.results
+  }
+}
+
+export default connect(mapStateToProps)(App)
