ffdd23cc531cacaa31afc9fe4eb6f37eb3e0ac62
Andreas Karpasitis <karpasitis@outlook.com>
2018-05-27 16:08:03 +0200

    implement processing of number to generate possible strings

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const path = require('path')
 const keys = require('./helpers/keys')
 const bodyParser = require('body-parser')
-
+const _ = require('lodash')
 
 const app = express()
+const reduceFunc = (a, b) => b.reduce((result, b) => result.concat(a.map(a => a + b)), []);
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(bodyParser.json({
@@ -12,13 +13,23 @@ app.use(bodyParser.json({
 }))
 
 app.post('/api/t9', (req, res) => {
-  console.log('T9 Get Request', req.body)
-  // handle T9 response here
+  console.log('T9 Post Request for: ', req.body.number)
+  const number = req.body.number
+  if (number <= 9) {
+    res.status(200).send({ data: keys[number] })
+  } else {
+    const combinations = _(String(number).split('')).map(digit => {
+      return keys[digit]
+    }).reduce(reduceFunc).sort()
+
+    res.status(200).send({ data: combinations })
+  }
 })
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
+
 
 const port = process.env.PORT || 5050
 app.listen(port)
