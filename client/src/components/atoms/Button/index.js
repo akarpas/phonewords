e1c29a46797df6c96e08cb55534a09babd18659f
Andreas Karpasitis <karpasitis@outlook.com>
2018-05-28 21:57:27 +0200

    update styling

--- client/src/components/atoms/Button/index.js
+++ client/src/components/atoms/Button/index.js
@@ -4,10 +4,7 @@ import { connect } from 'react-redux'
 import style from './index.scss'
 
 class Button extends Component {
-  state = {}
-
   render() {
-    console.warn(this.props)
     return (
       <div className={style.button} onClick={this.props.click}>
         <div className={style.label}>{this.props.label}</div>
