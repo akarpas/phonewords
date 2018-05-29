3817192cbf028b7abcfcfee7057db840c4803c9b
Andreas Karpasitis <karpasitis@outlook.com>
2018-05-29 22:30:48 +0200

    add logging for redux on development environment

--- client/src/store.js
+++ client/src/store.js
@@ -7,12 +7,12 @@ const logger = store => next => (action) => {
   const oldState = store.getState()
   const res = next(action)
   const newState = store.getState()
-  // if (process.env.NODE_ENV !== 'production') {
-  //   // eslint-disable-next-line
-  //   console.info('------ Action dispatched ------', action) // eslint-disable-next-line
-  //   console.info('---------- Old state ----------', oldState) // eslint-disable-next-line
-  //   console.info('---------- New state ----------', newState)
-  // }
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line
+    console.info('------ Action dispatched ------', action) // eslint-disable-next-line
+    console.info('---------- Old state ----------', oldState) // eslint-disable-next-line
+    console.info('---------- New state ----------', newState)
+  }
   return res
 }
 
