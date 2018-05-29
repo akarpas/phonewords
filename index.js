ba3c752a645d320a19659c970d561acf840d7557
Andreas Karpasitis <karpasitis@outlook.com>
2018-05-29 23:20:03 +0200

    add error checking for possible outcomes

--- index.js
+++ index.js
@@ -22,7 +22,20 @@ app.post('/api/t9', (req, res) => {
   const wordDictionary = dictionaryOne.concat(dictionaryTwo).split("\n")
 
   const number = req.body.number
-  if (number <= 9) {
+  const isNumberClean = !(String(number).includes('0') || String(number).includes('9'))
+  if (typeof number !== 'number') {
+    res.status(400).send({
+      error: "only numbers"
+    })
+  } else if (!isNumberClean) {
+    res.status(400).send({
+      error: "available only digits with text: 2, 3, 4, 5, 6, 7, 8"
+    })
+  } else if (String(number).length > 10) {
+    res.status(400).send({
+      error: "currently up to 10 digit numbers supported"
+    })
+  } else if (number <= 9) {
     res.status(200).send({
       combos: keys[number],
       words: keys[number]
