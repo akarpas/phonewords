b31724ab5de6290ebe030bbf69ae911fd69f8918
Andreas Karpasitis <andreas.karpasitis@utopiamusic.com>
2018-05-30 23:54:59 +0200

    implement phone button component

--- native/components/atoms/PhoneButton/index.js
+++ native/components/atoms/PhoneButton/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native'
+import { Button, Icon } from 'react-native-elements'
 
-class Button extends Component {
+class PhoneButton extends Component {
   getContent = () => {
     switch (this.props.label) {
       case 1:
@@ -8,7 +10,7 @@ class Button extends Component {
       case '*':
         return 'loop'
       case '#':
-        return 'delete_forever'
+        return 'delete-forever'
       default:
         return 'other'
     }
@@ -31,27 +33,66 @@ class Button extends Component {
     const hasIcon = this.props.sub === 'icon'
     const id = this.getId()
     const content = this.getContent()
-    const icon = hasIcon && (
-        <i
-          className="material-icons"
-          id={id}
-          style={{
-            fontSize: '12px',
-            height: '12px',
-          }}>
-          {content}
-        </i>
+
+    const contentRender = hasIcon ? (
+      <TouchableHighlight duration={1} style={styles.button} onPress={this.props.click} id={id}>
+      <View>
+        <Text style={styles.label}>
+          {this.props.label}
+        </Text>
+        <Icon name={content} color='white' size={12} />
+        </View>
+      </TouchableHighlight>
+    ) : (
+      <TouchableHighlight duration={1} style={styles.button} onPress={this.props.click} id={id}>
+            <View>
+
+        <Text style={styles.label}>
+          {this.props.label}
+        </Text>
+        <Text style={styles.sub}>
+          {this.props.sub}
+        </Text>
+        </View>
+      </TouchableHighlight>
     )
 
-    return (
-      <div className={style.button} onClick={this.props.click} id={id}>
-        <div className={style.label} id={id}>{this.props.label}</div>
-        <div className={style.sub} id={id}>
-          {hasIcon ? icon : this.props.sub}
-        </div>
-      </div>
-    )
+    return contentRender
   }
 }
 
-export default Button
+export default PhoneButton
+
+const styles = StyleSheet.create({
+  container: {
+    height: 80,
+    minWidth: 80,
+    maxWidth: 80
+  },
+  button: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: "#72b1db",
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: "#1f628d",
+    borderStyle: "solid",
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10
+  },
+  label: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "#ffffff"
+  },
+  sub: {
+    fontSize: 12,
+    textAlign: "center",
+    color: "#ffffff"
+  },
+  icon: {
+    height: 12
+  }
+})
